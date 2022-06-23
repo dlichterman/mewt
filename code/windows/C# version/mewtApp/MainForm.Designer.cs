30936eeaaf59a36e1325b5ab4743129bb68d27e8
Daniel Lichterman <dlichterman@gmail.com>
2022-06-22 20:36:41 -0700

    First commit of mewt app
    First commit. basic stuff is working, but I have not tested with a LED yet.

---
+++ code/windows/C# version/mewtApp/MainForm.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace mewtApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblStatusVal = new System.Windows.Forms.Label();
+            this.lblVol = new System.Windows.Forms.Label();
+            this.lblVolVal = new System.Windows.Forms.Label();
+            this.lblLastRecd = new System.Windows.Forms.Label();
+            this.lblLastRecdVal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(358, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Exit Application";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 9);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(42, 15);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "Status:";
+            // 
+            // lblStatusVal
+            // 
+            this.lblStatusVal.AutoSize = true;
+            this.lblStatusVal.Location = new System.Drawing.Point(78, 9);
+            this.lblStatusVal.Name = "lblStatusVal";
+            this.lblStatusVal.Size = new System.Drawing.Size(0, 15);
+            this.lblStatusVal.TabIndex = 2;
+            // 
+            // lblVol
+            // 
+            this.lblVol.AutoSize = true;
+            this.lblVol.Location = new System.Drawing.Point(12, 24);
+            this.lblVol.Name = "lblVol";
+            this.lblVol.Size = new System.Drawing.Size(50, 15);
+            this.lblVol.TabIndex = 3;
+            this.lblVol.Text = "Volume:";
+            // 
+            // lblVolVal
+            // 
+            this.lblVolVal.AutoSize = true;
+            this.lblVolVal.Location = new System.Drawing.Point(78, 24);
+            this.lblVolVal.Name = "lblVolVal";
+            this.lblVolVal.Size = new System.Drawing.Size(13, 15);
+            this.lblVolVal.TabIndex = 4;
+            this.lblVolVal.Text = "0";
+            // 
+            // lblLastRecd
+            // 
+            this.lblLastRecd.AutoSize = true;
+            this.lblLastRecd.Location = new System.Drawing.Point(12, 39);
+            this.lblLastRecd.Name = "lblLastRecd";
+            this.lblLastRecd.Size = new System.Drawing.Size(60, 15);
+            this.lblLastRecd.TabIndex = 5;
+            this.lblLastRecd.Text = "Last Recd:";
+            // 
+            // lblLastRecdVal
+            // 
+            this.lblLastRecdVal.AutoSize = true;
+            this.lblLastRecdVal.Location = new System.Drawing.Point(78, 39);
+            this.lblLastRecdVal.Name = "lblLastRecdVal";
+            this.lblLastRecdVal.Size = new System.Drawing.Size(0, 15);
+            this.lblLastRecdVal.TabIndex = 6;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 65);
+            this.Controls.Add(this.lblLastRecdVal);
+            this.Controls.Add(this.lblLastRecd);
+            this.Controls.Add(this.lblVolVal);
+            this.Controls.Add(this.lblVol);
+            this.Controls.Add(this.lblStatusVal);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Mewt App";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label lblStatus;
+        private Label lblStatusVal;
+        private Label lblVol;
+        private Label lblVolVal;
+        private Label lblLastRecd;
+        private Label lblLastRecdVal;
+    }
+}
---
+++ code/windows/C# version/mewtApp/MainForm.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CoreAudio;
+using System.IO.Ports;
+using System.Timers;
+
+namespace mewtApp
+{
+    public partial class MainForm : Form
+    {
+        private SerialPort port;
+        bool found = false;
+
+        System.Timers.Timer tmrDisconnectCheck;
+        System.Timers.Timer tmrVolume;
+
+        NotifyIcon trayIcon;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Minimized;
+            trayIcon = new NotifyIcon()
+            {
+                Icon = this.Icon,
+                Visible = true
+            };
+            trayIcon.MouseClick += new MouseEventHandler(iconOnClick);
+            this.ShowInTaskbar = false;
+        }
+
+        public void ChangeLabel(string msg, Label label)
+        {
+            if (label.InvokeRequired)
+                label.Invoke(new MethodInvoker(delegate { label.Text = msg; }));
+            else
+                label.Text = msg;
+        }
+
+        private void iconOnClick(object? sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.ShowInTaskbar = true;
+                this.Show();
+                
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Application.DoEvents();
+            Task.Delay(TimeSpan.FromMilliseconds(1000))
+                .ContinueWith(task => findDevice());
+        }
+
+        private void SetMute(bool muteState)
+        {
+            MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+            var q = enumerator.EnumerateAudioEndPoints(EDataFlow.eCapture, DEVICE_STATE.DEVICE_STATE_ACTIVE);
+            foreach (var ep in q)
+            {
+                foreach (var ep2 in ep.AudioSessionManager2.Sessions)
+                {
+                    ep2.SimpleAudioVolume.Mute = muteState;
+                }
+            }
+        }
+
+        private void setUpConnection(string comPort)
+        {
+            try
+            {
+                port.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+
+                // Begin communications
+                port.Write("101");
+
+                //Check current state to show correct color
+                MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+                var q = enumerator.GetDefaultAudioEndpoint(EDataFlow.eCapture, ERole.eCommunications);
+                bool muteState = q.AudioSessionManager2.Sessions.FirstOrDefault().SimpleAudioVolume.Mute;
+                //Invert the bool because 0 = muted
+                port.Write(muteState ? "0" : "1");
+
+                //timer to check if we're still connected
+                tmrDisconnectCheck = new System.Timers.Timer();
+                tmrDisconnectCheck.Interval = 2500;
+                tmrDisconnectCheck.Elapsed += tmrDisconnectCheck_Elapsed;
+                tmrDisconnectCheck.Start();
+
+                //Timer to send volume
+                tmrVolume = new System.Timers.Timer();
+                tmrVolume.Interval = 100;
+                tmrVolume.Elapsed += tmrVolume_elapsed;
+                tmrVolume.Start();
+            }
+            catch(Exception ex)
+            {
+                lostConnection();
+            }
+        }
+
+        private void tmrVolume_elapsed(object? sender, ElapsedEventArgs e)
+        {
+            MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+            var q = enumerator.GetDefaultAudioEndpoint(EDataFlow.eCapture, ERole.eCommunications);
+            int vol = (int)Math.Round(q.AudioMeterInformation.MasterPeakValue * 100);
+            try
+            {
+                if (vol > 0)
+                {
+                    port.Write(vol.ToString());
+                    ChangeLabel(vol.ToString(), lblVolVal);
+                }
+                else
+                {
+                    ChangeLabel("0", lblVolVal);
+                }
+            }
+            catch (Exception ex)
+            {
+                //
+            }
+        }
+
+        private void lostConnection()
+        {
+            if (tmrDisconnectCheck != null)
+            {
+                tmrDisconnectCheck.Stop();
+                tmrDisconnectCheck.Dispose();
+            }
+            if (tmrVolume != null)
+            {
+                tmrVolume.Stop();
+            }
+            found = false;
+            ChangeLabel("Lost connection", lblStatusVal);
+            //Console.Write("Lost connection");
+            findDevice();
+        }
+
+        private void tmrDisconnectCheck_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (!port.IsOpen)
+            {
+                lostConnection();
+            }
+        }
+
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // Show all the incoming data in the port's buffer
+            string s = port.ReadExisting();
+            if (s == "0\r\n")
+            {
+                SetMute(true);
+                ChangeLabel("Mute", lblLastRecdVal);
+            }
+            else if (s == "1\r\n")
+            {
+                SetMute(false);
+                ChangeLabel("Unmute", lblLastRecdVal);
+            }
+            Console.Write(s);
+        }
+
+        private void findDevice()
+        {
+            ChangeLabel("Searching for COM Port", lblStatusVal);
+            while (!found)
+            {
+                foreach (string s in SerialPort.GetPortNames())
+                {
+                    try
+                    {
+                        ChangeLabel("Trying port " + s, lblStatusVal);
+                        port = new SerialPort(s, 9600, Parity.None, 8, StopBits.One);
+                        port.DtrEnable = true;
+                        port.RtsEnable = true;
+                        port.ReadTimeout = 50;
+                        port.Open();
+                        port.DataReceived += new SerialDataReceivedEventHandler(port_searchDataRecd);
+                        Thread.Sleep(5000);
+                        if (!found)
+                        {
+                            port.Close();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        //
+                    }
+                }
+            }
+        }
+
+        private void port_searchDataRecd(object sender, SerialDataReceivedEventArgs e)
+        {
+            string dataRecd = port.ReadExisting();
+            if (dataRecd.Contains("IdentifyMewtDevice"))
+            {
+                ChangeLabel("Connected port " + ((SerialPort)sender).PortName, lblStatusVal);
+                //Console.Write("Connected port " + ((SerialPort)sender).PortName);
+                found = true;
+                port.Write("IdentifyMewtDevice");
+                Thread.Sleep(1000);
+                port.DataReceived -= new SerialDataReceivedEventHandler(port_searchDataRecd);
+                setUpConnection(((SerialPort)sender).PortName);
+            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                this.WindowState = FormWindowState.Minimized;
+                this.ShowInTaskbar = false;
+            }
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+                this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
